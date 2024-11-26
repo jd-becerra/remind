03241f8ae6988b335360b4c3fc9a752972d64b79
CyrusSmith2099 <148400158+CyrusSmith2099@users.noreply.github.com>
2024-11-26 14:43:40 -0600

    Add skip button functionality and improve animation handling in level 1

--- escenas/Niveles/Nivel1/lvl_1.gd
+++ escenas/Niveles/Nivel1/lvl_1.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @onready var camera_animation: Camera2D = %Camara_Animacion
 @onready var camera_player: Camera2D = %Camera_Jugador
+@onready var animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var animation_player = $AnimationPlayer
+	
 	animation_player.play("Inicio")
+	%Skip.pressed.connect(_on_skip_pressed)
 	
 	# Set camera_animation as the current camera
 	camera_animation.enabled = true
@@ -14,8 +16,19 @@ func _ready() -> void:
 
 	animation_player.animation_finished.connect(_on_inicio_animation_finished)
 
-func _on_inicio_animation_finished() -> void:
+func _on_inicio_animation_finished(_anim_name) -> void:
 	# Set camera_player as the current camera
 	camera_animation.enabled = false
 	camera_player.enabled = true
+<<<<<<< HEAD
 	
+=======
+	%Protagonista_Animacion.visible = false
+	%Skip.visible = false
+	%Player.visible = true
+
+func _on_skip_pressed() -> void:
+	# Jump to the end of the animation
+	var anim_len = animation_player.current_animation_length
+	animation_player.seek(anim_len - 0.1, true)
+>>>>>>> 944799a (Add skip button functionality and improve animation handling in level 1)
